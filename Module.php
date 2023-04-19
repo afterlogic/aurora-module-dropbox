2c8f02f519c57aea9ed826f54c248cdec50a7ea8
Vasil Sokolov <vasil@afterlogic.com>
2023-04-19 21:42:37 +0200

    Refactored getting module settings (part2)

--- Module.php
+++ Module.php
@@ -14,6 +14,8 @@ namespace Aurora\Modules\Dropbox;
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
+ * @property Settings $oModuleSettings
+ *
  * @package Modules
  */
 class Module extends \Aurora\System\Module\AbstractModule
@@ -101,9 +103,9 @@ class Module extends \Aurora\System\Module\AbstractModule
             $aResult = array(
                 'Name' => $this->sService,
                 'DisplayName' => self::GetName(),
-                'EnableModule' => $this->getConfig('EnableModule', false),
-                'Id' => $this->getConfig('Id', ''),
-                'Secret' => $this->getConfig('Secret', '')
+                'EnableModule' => $this->oModuleSettings->EnableModule,
+                'Id' => $this->oModuleSettings->Id,
+                'Secret' => $this->oModuleSettings->Secret
             );
         }
 
@@ -114,7 +116,7 @@ class Module extends \Aurora\System\Module\AbstractModule
                 $oAccount = $oOAuthIntegratorWebclientDecorator->GetAccount($this->sService);
             }
             $aResult = array(
-                'EnableModule' => $this->getConfig('EnableModule', false),
+                'EnableModule' => $this->oModuleSettings->EnableModule,
                 'Connected' => $oAccount ? true : false
             );
             $aArgs = array(
